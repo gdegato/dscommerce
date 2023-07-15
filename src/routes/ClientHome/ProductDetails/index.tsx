17741f8f40a25b3113a232c1efc99399b20b0730
gdegato <rosele.tst@gmail.com>
2023-07-15 10:22:29 -0300

    Links, catalog, product details

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,35 +2,29 @@ import './styles.css'
 import ProductDetailsCard from '../../../components/ProductDetailsCard'
 import ButtonPrimary from '../../../components/ButtonPrimary'
 import ButtonInverse from '../../../components/ButtonInverse'
-import { ProductDTO } from '../../../models/product'
-
-const product: ProductDTO = {
-    id: 2,
-    name: 'Smart TV',
-    description: "lorem lorem lorem",
-    imgUrl: 'https://a-static.mlcdn.com.br/800x560/smart-tv-43-uhd-4k-led-samsung-43cu7700-lancamento-2023-wi-fi-bluetooth-alexa-3-hdmi/magazineluiza/237016000/0d04b4d2c6a502b6e928766eb8c44c93.jpg',
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: 'Eletronicos'
-        },
-        {
-            id: 3,
-            name: 'Computadores'
-        }
-    ]
-}
+import * as productService from '../../../services/product-service'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ProductDetails() {
+
+    const params = useParams();
+
+    const product = productService.findById(Number(params.productId))
     return (
         <>
             <main>
                 <section id="product-details-section" className="dsc-container">
-                    <ProductDetailsCard product={product} />
+                    {
+                        product &&
+                        <ProductDetailsCard product={product} />
+                    }
                     <div className="dsc-btn-page-container">
                         <ButtonPrimary text='Comprar' />
-                        <ButtonInverse text='Inicio' />
+                        <Link to='/'>
+                            <ButtonInverse text='Inicio' />
+                        </Link>
+
                     </div>
                 </section>
             </main>
