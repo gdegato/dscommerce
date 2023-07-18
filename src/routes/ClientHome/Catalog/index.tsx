258cb0552fadb00c2316c666ea5ca51941b4cbd9
gdegato <rosele.tst@gmail.com>
2023-07-18 11:27:07 -0300

    First backend requests

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,10 +2,26 @@ import './styles.css'
 import SearchBar from '../../../components/SearchBar'
 import CatalogCard from '../../../components/CatalogCard'
 import ButtonNextPage from '../../../components/ButtonNextPage'
+import { useEffect, useState } from 'react'
+import { ProductDTO } from '../../../models/product'
 import * as productService from '../../../services/product-service'
 
 
 export default function Catalog() {
+    const [product, setProduct] = useState<ProductDTO[]>([]);
+
+    useEffect(() => {
+      productService.findAll()
+            .then(response => {
+                setProduct(response.data.content)
+            }).catch(
+                response => {
+                console.error(response)
+            })
+    }, [])
+
+
+
     return (
         <>
             <main>
@@ -13,8 +29,7 @@ export default function Catalog() {
                     <SearchBar />
                     <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                         {
-                            productService
-                                .findAll()
+                            product
                                 .map(
                                     product =>
                                         <CatalogCard
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,16 +1,31 @@
 import './styles.css'
 import ProductDetailsCard from '../../../components/ProductDetailsCard'
+import * as productService from '../../../services/product-service'
 import ButtonPrimary from '../../../components/ButtonPrimary'
 import ButtonInverse from '../../../components/ButtonInverse'
-import * as productService from '../../../services/product-service'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { ProductDTO } from '../../../models/product'
 
 export default function ProductDetails() {
 
     const params = useParams();
 
-    const product = productService.findById(Number(params.productId))
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        productService.findById(Number(params.productId))
+            .then(response => {
+                setProduct(response.data)
+            }).catch(
+                response => {
+                    console.error(response)
+                }
+            );
+
+    }, [])
+
     return (
         <>
             <main>
