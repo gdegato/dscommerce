78a43f8693995a37e685d73f7fcd41a39d191146
gdegato <rosele.tst@gmail.com>
2023-07-23 21:02:34 -0300

    Cart mock data

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,19 +5,32 @@ import ButtonNextPage from '../../../components/ButtonNextPage'
 import { useEffect, useState } from 'react'
 import { ProductDTO } from '../../../models/product'
 import * as productService from '../../../services/product-service'
+import { CategoryDTO } from '../../../models/category'
 
 
 export default function Catalog() {
     const [product, setProduct] = useState<ProductDTO[]>([]);
 
+    const objTest: CategoryDTO = {
+        id: 8,
+        name: 'Jardinagem'
+    }
+
     useEffect(() => {
-      productService.findAll()
+        //local storage tem q ser string, dai tem q converter de obj para string
+        /*  localStorage.setItem('minhaCategoria', JSON.stringify(objTest)) */
+
+        //Aqui a conversão é o contrario para pegar o dado do storage
+        /*   const obj = JSON.parse(localStorage.getItem('minhaCategoria') || '{}')
+          console.log(obj.name) */
+
+        productService.findAll()
             .then(response => {
                 setProduct(response.data.content)
             }).catch(
                 error => {
-                console.error(error)
-            })
+                    console.error(error)
+                })
     }, [])
 
 
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -27,7 +27,6 @@ export default function ProductDetails() {
 
                 }
             );
-
     }, [])
 
     return (
