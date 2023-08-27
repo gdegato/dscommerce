1a230077f7bcba495167e88b3de92f4b778213bf
gdegato <rosele.tst@gmail.com>
2023-08-27 20:42:13 -0300

    Forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -6,3 +7,10 @@ export function update(inputs: any, name: string, newValue: any) {
     return {...inputs, [name]: {...inputs[name], value: newValue }};
 }
     
+export function toValues(inputs: any) {
+    const data: any = {};
+    for (let name in inputs) {
+        data[name] = inputs[name].value;
+    }
+    return data;
+}
