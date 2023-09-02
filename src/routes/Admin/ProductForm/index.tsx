0c0957e22f4473094a8d20d680f6895982b3c5cd
gdegato <rosele.tst@gmail.com>
2023-09-02 17:56:26 -0300

    Estilização do Selector

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -15,6 +15,7 @@ import * as categoryService from '../../../services/category-service'
 import FormTextArea from '../../../components/FormTextArea'
 import { CategoryDTO } from '../../../models/category'
 import FormSelect from '../../../components/FormSelect'
+import { selectStyles } from '../../../utils/select'
 
 
 
@@ -143,8 +144,9 @@ function ProductForm() {
                             </div>
                             <div>
                                 <FormSelect
-                                    className="dsc-form-control"
+                                    className="dsc-form-control dsc-form-select-container"
                                     {...formData.categories}
+                                    styles={selectStyles}
                                     options={categories}
                                     onChange={(obj: any) => {
                                         const newFormData = forms.updateAndValidate(formData, "categories", obj);
